c28e7f9f41595218b569de5884dfa812a622d022
abs8090 <aalhomai@stevens.edu>
2017-09-17 01:47:49 -0400

    I printed the output without styling. I'll do that tomorrow

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.awt.List;
+import java.util.ArrayList;
+
 import classes.Family;
 import classes.Gedcom;
 import classes.Individual;
+import classes.IndividualRelFamily;
 import utils.DBUtils;
 import utils.FileUtils;
 
@@ -17,6 +21,73 @@ public class Main {
             System.out.println(family.members.size());
 
             System.out.println(Family.all().size());
+            
+            ArrayList<Individual> AllIndividuals = (ArrayList)Individual.all(); //begining of printing individuals
+            System.out.println("Individuals: ");
+            
+            for(int i = 0; i < AllIndividuals.size(); i++){
+            	
+            	String spouse = " ";
+            	String child = " ";
+            	
+            	System.out.print(AllIndividuals.get(i).id + " " +
+            			AllIndividuals.get(i).name + " " +
+            			AllIndividuals.get(i).gender + " " +
+            			AllIndividuals.get(i).birthday + " " +
+            			AllIndividuals.get(i).age + " " +
+            			AllIndividuals.get(i).isAlive + " " +
+            			((AllIndividuals.get(i).death == null) ?"NA": AllIndividuals.get(i).death) + " " );
+            	
+            	System.out.println();
+            }
+            
+            
+            
+            // end of printing individuals
+            
+            ArrayList<Family> AllFamilies = (ArrayList)Family.all(); // beginning of printing families
+            
+            System.out.println("Families: ");
+            for(int i = 0; i < AllFamilies.size(); i++){
+            	
+            	Family Cfamily = AllFamilies.get(i); // current family
+            	System.out.print(Cfamily.id + " " + 
+            			((Cfamily.married == null) ?"NA":Cfamily.married) + " " +
+            	
+            			((Cfamily.divorced == null) ?"NA":Cfamily.divorced)
+            			+" ");
+            	
+            	ArrayList<IndividualRelFamily> famMembers = (ArrayList)Cfamily.members;
+            	
+            	String hID = "";
+            	String wID = "";
+            	ArrayList<String> chiID = new ArrayList<String>();
+            	
+            	for(int j = 0; j < famMembers.size(); j++){
+            		
+            		if(famMembers.get(j).role.equals("H")){
+            			hID = famMembers.get(j).individualId;
+            		
+            		}else if(famMembers.get(j).role.equals("W")){
+            			wID = famMembers.get(j).individualId;
+            			
+            		}else if(famMembers.get(j).role.equals("C")){
+            			chiID.add(famMembers.get(j).individualId);
+            		}
+            		
+            	}
+            	
+            	System.out.print(hID + " " + Individual.findById(hID).name + " " + wID + " " + Individual.findById(wID).name);
+            	
+            	for(int z = 0; z < chiID.size(); z++){
+            		
+            		System.out.print(" "+ chiID.get(z) + " ");
+            	}
+            	System.out.println();
+            }// end of printing families
+            
+            
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/classes/Individual.java
+++ src/classes/Individual.java
@@ -3,6 +3,11 @@ package classes;
 import utils.DBUtils;
 
 import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,15 +19,41 @@ public class Individual {
     public String gender;
     public Date birthday;
     public Date death;
+    public int age;
+    public boolean isAlive;
 
+    
+    
     public Individual(String id, String name, String gender, Date birthday, Date death) {
         this.id = id;
         this.name = name;
         this.gender = gender;
         this.birthday = birthday;
         this.death = death;
-    }
+        
+        this.isAlive = (this.death == null) ? true : false;
+        
+        // calculate each individual age
+        if(isAlive){
+        	LocalDate today = LocalDate.now();
+        	LocalDate birth = new java.sql.Date(birthday.getTime()).toLocalDate();
 
+        	Period p = Period.between(birth, today);
+		
+        	this.age = p.getYears();
+        	
+        }else {
+        	
+        	LocalDate dayOfDeath = new java.sql.Date(death.getTime()).toLocalDate();
+        	LocalDate birth = new java.sql.Date(birthday.getTime()).toLocalDate();
+
+        	Period p = Period.between(birth, dayOfDeath);
+		
+        	this.age = p.getYears();
+        	
+        }
+    }
+    
     public static List<Individual> all() throws Exception {
         String sql = "select * from individuals";
         ResultSet resultSet = DBUtils.query(sql);
