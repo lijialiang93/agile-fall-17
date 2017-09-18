53e28cf30fec40e5b83c993edfb4eef21815dd7c
abs8090 <aalhomai@stevens.edu>
2017-09-18 00:12:43 -0400

    printing individual and family tables. Added children and spous columns to individual table

--- src/Main.java
+++ src/Main.java
@@ -29,44 +29,94 @@ public class Main {
             System.out.println(table);
 
 
-            ArrayList<Individual> AllIndividuals = (ArrayList) Individual.all(); //begining of printing individuals
-            System.out.println("Individuals: ");
-
+            ArrayList<Individual> AllIndividuals = (ArrayList) Individual.all();
+            ArrayList<Family> AllFamilies = (ArrayList) Family.all();
+            
+          //begining of printing individuals            
+            PrettyTable Itable = new PrettyTable("ID", "Name", "Gender", "Birthday", "Age"
+            		,"Alive", "Death", "Child", "Spouse");
+            
             for (int i = 0; i < AllIndividuals.size(); i++) {
 
-                String spouse = " ";
-                String child = " ";
+            	ArrayList<String> spouse = new ArrayList<String>();
+            	ArrayList<String> children = new ArrayList<String>();
+            	String spList = "{";
+            	String chList = "{";
+            	
+                for(int j = 0; j < AllFamilies.size(); j++){
+                	ArrayList<IndividualRelFamily> famMembers = (ArrayList) AllFamilies.get(j).members;
+                	
+                	for(int z = 0; z < famMembers.size(); z++){
+                		
+                		if(famMembers.get(z).individualId == AllIndividuals.get(i).id){
+                            
+                			if (famMembers.get(z).role.equals("H") || famMembers.get(z).role.equals("W")) {
+                            	spouse.add(AllFamilies.get(j).id);
 
-                System.out.print(AllIndividuals.get(i).id + " " +
-                        AllIndividuals.get(i).name + " " +
-                        AllIndividuals.get(i).gender + " " +
-                        AllIndividuals.get(i).birthday + " " +
-                        AllIndividuals.get(i).age + " " +
-                        AllIndividuals.get(i).isAlive + " " +
-                        ((AllIndividuals.get(i).death == null) ? "NA" : AllIndividuals.get(i).death) + " ");
+                            } else if (famMembers.get(z).role.equals("C")) {
+                            	children.add(AllFamilies.get(j).id);
+                            }
+                		}
+                	}
+                	
+                }
+                
+                for (int z = 0; z < spouse.size(); z++) {
 
-                System.out.println();
+                	// getting list of all souses in this family
+                    spList += spouse.get(z);
+                    
+                    if(z != spouse.size() -1){
+                    	spList += ", ";
+                    }
+                }
+                spList += "}";
+                
+                for (int z = 0; z < children.size(); z++) {
+
+                	// getting list of all children in this family
+                    chList += children.get(z);
+                    
+                    if(z != children.size() -1){
+                    	chList += ", ";
+                    }
+                }
+                chList += "}";
+                
+                Itable.addRow(AllIndividuals.get(i).id, AllIndividuals.get(i).name, AllIndividuals.get(i).gender,
+                		sdf.format(AllIndividuals.get(i).birthday),
+                		""+AllIndividuals.get(i).age, ""+AllIndividuals.get(i).isAlive,
+                		((AllIndividuals.get(i).death == null) ? "NA" : sdf.format(AllIndividuals.get(i).death)),
+                		chList, spList);
+                
+//                System.out.print(AllIndividuals.get(i).id + " " +
+//                        AllIndividuals.get(i).name + " " +
+//                        AllIndividuals.get(i).gender + " " +
+//                        AllIndividuals.get(i).birthday + " " +
+//                        AllIndividuals.get(i).age + " " +
+//                        AllIndividuals.get(i).isAlive + " " +
+//                        ((AllIndividuals.get(i).death == null) ? "NA" : AllIndividuals.get(i).death) + " ");
+//
+//                System.out.println();
+                
             }
-
-
-            // end of printing individuals
-
-            ArrayList<Family> AllFamilies = (ArrayList) Family.all(); // beginning of printing families
-
+         // end of printing individuals
+                       
             System.out.println("Families: ");
+         // beginning of printing families
+            PrettyTable Ftable = new PrettyTable("ID", "Married", "Divorced", "Husband ID", "Husband Name"
+            		,"Wife ID", "Wife Name", "Children");
+            
             for (int i = 0; i < AllFamilies.size(); i++) {
 
                 Family Cfamily = AllFamilies.get(i); // current family
-                System.out.print(Cfamily.id + " " +
-                        ((Cfamily.married == null) ? "NA" : Cfamily.married) + " " +
-
-                        ((Cfamily.divorced == null) ? "NA" : Cfamily.divorced)
-                        + " ");
 
                 ArrayList<IndividualRelFamily> famMembers = (ArrayList) Cfamily.members;
 
                 String hID = "";
                 String wID = "";
+                String ChList = "{";
+                
                 ArrayList<String> chiID = new ArrayList<String>();
 
                 for (int j = 0; j < famMembers.size(); j++) {
@@ -83,14 +133,26 @@ public class Main {
 
                 }
 
-                System.out.print(hID + " " + Individual.findById(hID).name + " " + wID + " " + Individual.findById(wID).name);
-
                 for (int z = 0; z < chiID.size(); z++) {
 
-                    System.out.print(" " + chiID.get(z) + " ");
+                	// getting list of all children in this family
+                    ChList += chiID.get(z);
+                    
+                    if(z != chiID.size() -1){
+                    	ChList += ", ";
+                    }
                 }
-                System.out.println();
+                ChList += "}";
+                
+                Ftable.addRow(Cfamily.id, 
+                		Cfamily.married != null? sdf.format(Cfamily.married): "NA",
+                		Cfamily.divorced != null? sdf.format(Cfamily.divorced): "NA", 
+                		 hID,Individual.findById(hID).name,
+                		wID, Individual.findById(wID).name,
+                		ChList);
+                //System.out.println();
             }
+            System.out.println(Ftable);
 
             // end of printing families
 
