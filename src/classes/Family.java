bfe65e2409b45da8c4982fb629728b1358c49d01
lijialiang93 <lijialiang93@gmail.com>
2017-09-26 02:39:09 -0400

    Change for sprint 1

--- src/classes/Family.java
+++ src/classes/Family.java
@@ -9,62 +9,68 @@ import java.util.List;
 
 public class Family {
 
-    public int id;
-    public Date married;
-    public Date divorced;
-    public List<IndividualRelFamily> members;
+	public int id;
+	public Date married;
+	public Date divorced;
+	public List<IndividualRelFamily> members;
 
-    public Family(int id, Date married, Date divorced) {
-        this.id = id;
-        this.married = married;
-        this.divorced = divorced;
-    }
+	public Family(int id, Date married, Date divorced) throws Exception {
+		this.id = id;
+		this.married = dateNotAfterCurrentDate(married, "Married", id);
+		this.divorced = dateNotAfterCurrentDate(divorced, "Divorced", id);
+	}
 
-    public static List<Family> all() throws Exception {
-        String sql = "select * from families";
-        ResultSet resultSet = DBUtils.query(sql);
+	public static List<Family> all() throws Exception {
+		String sql = "select * from families";
+		ResultSet resultSet = DBUtils.query(sql);
 
-        List<Family> res = new ArrayList<>();
-        while (resultSet.next()) {
-            Family family = new Family(
-                    resultSet.getInt(1),
-                    resultSet.getDate(2),
-                    resultSet.getDate(3)
-            );
+		List<Family> res = new ArrayList<>();
+		while (resultSet.next()) {
+			Family family = new Family(resultSet.getInt(1), resultSet.getDate(2), resultSet.getDate(3));
 
-            family.members = IndividualRelFamily.findBy("family_id", String.valueOf(family.id));
-            res.add(family);
-        }
-        return res;
-    }
+			family.members = IndividualRelFamily.findBy("family_id", String.valueOf(family.id));
+			res.add(family);
+		}
+		return res;
+	}
 
-    public static Family findById(int id) throws Exception {
-        String sql = "select * from families where id = ?";
-        ResultSet resultSet = DBUtils.query(sql, id);
+	public static Family findById(int id) throws Exception {
+		String sql = "select * from families where id = ?";
+		ResultSet resultSet = DBUtils.query(sql, id);
 
-        Family family = null;
-        if (resultSet.next()) {
-            family = new Family(
-                    resultSet.getInt(1),
-                    resultSet.getDate(2),
-                    resultSet.getDate(3)
-            );
+		Family family = null;
+		if (resultSet.next()) {
+			family = new Family(resultSet.getInt(1), resultSet.getDate(2), resultSet.getDate(3));
 
-            family.members = IndividualRelFamily.findBy("family_id", String.valueOf(family.id));
-        }
-        return family;
-    }
+			family.members = IndividualRelFamily.findBy("family_id", String.valueOf(family.id));
+		}
+		return family;
+	}
 
-    public void addMember(IndividualRelFamily member) {
-        if (members == null) {
-            members = new ArrayList<>();
-        }
-        members.add(member);
-    }
+	public void addMember(IndividualRelFamily member) {
+		if (members == null) {
+			members = new ArrayList<>();
+		}
+		members.add(member);
+	}
 
-    public void save() throws Exception {
-        String sql = "insert into families(id, married, divorced) values(?, ?, ?)";
-        DBUtils.update(sql, this.id, this.married, this.divorced);
-    }
+	// check date validity
+	public Date dateNotAfterCurrentDate(Date date, String type, int id) throws Exception {
+		if (date == null) {
+			return date;
+		} else {
+			Date currentDate = new Date();
+			int check = currentDate.compareTo(date);
+			if (check == 0 || check >= 0) {
+				return date;
+			}
+		}
+		throw new Exception(type + " should not be after the current date " + "Family ID: " + id);
+	}
+
+	public void save() throws Exception {
+		String sql = "insert into families(id, married, divorced) values(?, ?, ?)";
+		DBUtils.update(sql, this.id, this.married, this.divorced);
+	}
 
 }
--- src/classes/Individual.java
+++ src/classes/Individual.java
@@ -4,93 +4,99 @@ import utils.DBUtils;
 import utils.DateTimeUtils;
 
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Individual {
 
-    public int id;
-    public String name;
-    public String gender;
-    public Date birthday;
-    public Date death;
-    public int age;
-    public boolean isAlive;
-    
-    public Individual(int id, String name, String gender, Date birthday, Date death) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.birthday = birthday;
-        this.death = death;
+	public int id;
+	public String name;
+	public String gender;
+	public Date birthday;
+	public Date death;
+	public int age;
+	public boolean isAlive;
 
-        this.isAlive = this.death == null;
-        this.age = DateTimeUtils.calculateAge(this.birthday, this.death);
-    }
-    
-    public static List<Individual> all() throws Exception {
-        String sql = "select * from individuals order by id";
-        ResultSet resultSet = DBUtils.query(sql);
+	public Individual(int id, String name, String gender, Date birthday, Date death) throws Exception {
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.birthday = dateNotAfterCurrentDate(birthday, "Birthday", id);
+		this.death = dateNotAfterCurrentDate(death, "Death", id);
 
-        List<Individual> res = new ArrayList<>();
-        while (resultSet.next()) {
-            res.add(new Individual(
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(3),
-                    resultSet.getDate(4),
-                    resultSet.getDate(5)
-            ));
-        }
-        return res;
-    }
+		this.isAlive = this.death == null;
+		this.age = DateTimeUtils.calculateAge(this.birthday, this.death);
+	}
 
-    public static Individual findById(int id) throws Exception {
-        String sql = "select * from individuals where id = ?";
-        ResultSet resultSet = DBUtils.query(sql, id);
+	public static List<Individual> all() throws Exception {
+		String sql = "select * from individuals order by id";
+		ResultSet resultSet = DBUtils.query(sql);
 
-        Individual individual = null;
-        if (resultSet.next()) {
-            individual = new Individual(
-                    resultSet.getInt(1),
-                    resultSet.getString(2),
-                    resultSet.getString(3),
-                    resultSet.getDate(4),
-                    resultSet.getDate(5)
-            );
-        }
+		List<Individual> res = new ArrayList<>();
+		while (resultSet.next()) {
+			res.add(new Individual(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3),
+					resultSet.getDate(4), resultSet.getDate(5)));
+		}
+		return res;
+	}
 
-        return individual;
-    }
-    
-    // Abdul start    
-    public static List<Individual> borninLast30Days(List<Individual> ind){
-    	 
-    	List<Individual> indiBornIn30Days = new ArrayList<>();    	
-    	for(int x = 0; x < ind.size(); x++){    		
-    		if (DateTimeUtils.calculateAgeInDays(ind.get(x).birthday) <= 30){   			
-    			indiBornIn30Days.add(ind.get(x));
-    		}
-    	}    	
-    	return indiBornIn30Days;
-    }
-    
-    @Override
-    public boolean equals(Object obj){
-    	Individual compareAgainstIndi = (Individual)obj;
-    	
-    	if(this.id == compareAgainstIndi.id){
-    		return true;
-    	}
-    	return false;
-    }
-    
-    // Abdul end
+	public static Individual findById(int id) throws Exception {
+		String sql = "select * from individuals where id = ?";
+		ResultSet resultSet = DBUtils.query(sql, id);
 
-    public void save() throws Exception {
-        String sql = "insert into individuals(id, name, gender, birthday, death) values(?, ?, ?, ?, ?)";
-        DBUtils.update(sql, this.id, this.name, this.gender, this.birthday, this.death);
-    }
+		Individual individual = null;
+		if (resultSet.next()) {
+			individual = new Individual(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3),
+					resultSet.getDate(4), resultSet.getDate(5));
+		}
+
+		return individual;
+	}
+
+	// Abdul start
+	public static List<Individual> borninLast30Days(List<Individual> ind) {
+
+		List<Individual> indiBornIn30Days = new ArrayList<>();
+		for (int x = 0; x < ind.size(); x++) {
+			if (DateTimeUtils.calculateAgeInDays(ind.get(x).birthday) <= 30) {
+				indiBornIn30Days.add(ind.get(x));
+			}
+		}
+		return indiBornIn30Days;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Individual compareAgainstIndi = (Individual) obj;
+
+		if (this.id == compareAgainstIndi.id) {
+			return true;
+		}
+		return false;
+	}
+
+	// Abdul end
+
+	// check date validity
+	public Date dateNotAfterCurrentDate(Date date, String type, int id) throws Exception {
+		if (date == null) {
+			return date;
+		} else {
+			Date currentDate = new Date();
+			int check = currentDate.compareTo(date);
+			if (check == 0 || check >= 0) {
+				return date;
+			}
+		}
+		throw new Exception(type + " should not be after the current date " + "Individual ID: " + id);
+	}
+
+	public void save() throws Exception {
+		String sql = "insert into individuals(id, name, gender, birthday, death) values(?, ?, ?, ?, ?)";
+		DBUtils.update(sql, this.id, this.name, this.gender, this.birthday, this.death);
+	}
 
 }
